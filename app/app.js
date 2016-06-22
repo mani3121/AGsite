a2fb705e1241070428b0fdb070ac8910e6a20523
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2016-06-21 22:46:46 -0700

    'addedlogin'

--- app/app.js
+++ app/app.js
@@ -1,1 +1,24 @@
-angular.module('mainApp',['ui.bootstrap']);
+var apApp = angular.module('mainApp',['ui.bootstrap','slick','ui.router']);
+apApp.config(function($stateProvider, $urlRouterProvider) {
+  //$stateProvider & $urlRouterProvider should be injected in the same manner
+  //don't forget to add angular-ui-router.js in your index file
+  // 'ui.router' should be injected to the main module
+  console.log($stateProvider)
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('course.tpl.html',{
+    url: "/Course",
+    templateUrl: "app/partilals/course.tpl.html",
+    controller: "mainCtrl"
+  })
+  .state('about',{
+    url: "/AboutUs",
+    templateUrl: "app/partilals/aboutUs.tpl.html",
+	controller:"aboutUsCtrl"
+  })
+  .state('home',{
+    url: "/",
+    templateUrl: "app/partilals/home.tpl.html",
+	controller: "mainCtrl"
+  });
+});
